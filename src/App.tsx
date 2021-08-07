f40553727bfa07c6c6427521ac711074c4fdc915
Apurv Jain <apurv.jain@flipkart.com>
2021-08-07 21:26:23 +0530

    Goal Calculator ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React from "react";
 import "./App.css";
-import Status from "./components/Status";
-import { Flex1Row } from "./components/@styled/BaseElements";
+import GoalEstimator from "./components/GoalEstimator";
 
 function App() {
   return (
     <div className="App">
-      <div className="appHeading">Task Board</div>
-      <Flex1Row className="username">UserName</Flex1Row>
-      <Flex1Row
-        style={{
-          minHeight: "90%",
-          overflowX: "scroll",
-        }}
-      >
-        <Status />
-      </Flex1Row>
+      <GoalEstimator />
     </div>
   );
 }
